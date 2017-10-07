bb56f17e478998d3e74dad482dfad48756aded07
Rohan Verma <rohanverma2004@gmail.com>
2017-10-07 15:18:25 +0530

    feat: conversion real currency to real

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import requests as re
 from flask import Flask, request, render_template, session, redirect, abort, flash, jsonify
 import sys
 import logging
@@ -10,10 +10,29 @@ app.logger.setLevel(logging.ERROR)
 
 app.secret_key = 'secret'
 
+# xe.com
+xe_account_id = 'student926567212'
+xe_api_key = 'a77skb1r13cnrhjg5j8953nuvj'
+
 
 @app.route('/')
 def index():
 	return "Hello World"
+
+
+@app.route('/convert_rr/<val>/<_from>/<_to>', methods=['GET'])
+def convert_real_to_real(val, _from, _to):
+	if request.method == 'GET':
+		params = (
+			('from', _from),
+			('to', _to),
+			('amount', val),
+		)
+
+		resp = re.get('https://xecdapi.xe.com/v1/convert_from.json/', params=params, auth=(xe_account_id, xe_api_key))
+		result = {"result": resp.json()["to"][0]["mid"], 'status': 'OK'}
+
+		return jsonify(result)
 
 
 @app.errorhandler(404)
@@ -22,4 +41,5 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	port = int(os.environ.get("PORT", 5050))
+	app.run(host='0.0.0.0', port=port, debug=True)
