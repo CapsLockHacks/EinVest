e9feeaa980c28b89d671db382dd7f72aeb418309
Rohan Verma <rohanverma2004@gmail.com>
2017-10-08 03:48:14 +0530

    feat: scheduler check predictions

--- scheduler.sh
+++ scheduler.sh
@@ -1,1 +1,8 @@
-curl http://localhost:5050/check_predictions/
+while :
+do
+	echo "Bot Running! Press [CTRL+C] to stop.."
+	curl http://10.1.24.70:5050/check_predictions/
+	echo "Execution of Orders Executed"
+
+	sleep 60
+done
