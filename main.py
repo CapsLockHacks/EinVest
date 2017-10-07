69de1b7eab8d6141225b97c496db5bc130c166e7
Rohan Verma <rohanverma2004@gmail.com>
2017-10-08 01:33:45 +0530

    feat: create market

--- main.py
+++ main.py
@@ -105,6 +105,121 @@ def convert_real_to_real(val, _from, _to):
 		return jsonify(result)
 
 
+@app.route('/create_market/<stock>/')
+def create_market(stock):
+	if request.method == 'GET':
+		file_contents = """
+const Gnosis = require('@gnosis.pm/gnosisjs');
+const Web3 = require('web3');
+
+const resolutionDate = new Date();
+resolutionDate.setDate(resolutionDate.getDate() + 1);
+
+const options = {
+  ethereum: new Web3(new Web3.providers.HttpProvider('http://localhost:8545')).currentProvider,
+  ipfs: {
+    host: 'localhost',
+    port: 5001,
+    protocol: 'http'
+  }
+};
+
+const eventDescription = {
+    title: '"""+stock+"""',
+    description: 'My first sample market.',
+    resolutionDate: resolutionDate.toISOString(),
+    outcomes: ['Yes', 'No'],
+};
+
+let gnosisInstance;
+let ipfsHash;
+let oracle;
+let categoricalEvent;
+let market;
+
+Gnosis.create(options)
+.then(result => {
+    gnosisInstance = result;
+    console.info('[GnosisJS] > connection established');
+    console.info("[GnosisJS] > Creating event description...");
+    gnosisInstance.publishEventDescription(eventDescription)
+    .then(result => {
+      ipfsHash = result;
+      console.info("[GnosisJS] > Event description hash: " + ipfsHash);
+      console.info("[GnosisJS] > Creating Centralized Oracle...");
+      gnosisInstance.createCentralizedOracle(ipfsHash)
+      .then(result => {
+        oracle = result;
+        console.info("[GnosisJS] > Centralized Oracle was created");
+        console.info("[GnosisJS] > Creating Categorical Event...");
+        gnosisInstance.createCategoricalEvent({
+            collateralToken: gnosisInstance.etherToken,
+            oracle,
+            // Note the outcomeCount must match the length of the outcomes array published on IPFS
+            outcomeCount: 2,
+        })
+        .then(result => {
+          categoricalEvent = result;
+          console.info("[GnosisJS] > Categorical event was created");
+          console.info("[GnosisJS] > Creating market...");
+          // console.info(gnosisInstance);
+          gnosisInstance.createMarket({
+              event: categoricalEvent,
+              marketMaker: gnosisInstance.lmsrMarketMaker,
+              marketFactory: gnosisInstance.standardMarketFactory,
+              fee: 50000
+          })
+          .then(response => {
+            market = response;
+            console.info("[GnosisJS] > Market was created");
+            Promise.all([
+                gnosisInstance.etherToken.deposit({ value: 4e18 }),
+                gnosisInstance.etherToken.approve(market.address, 4e18),
+                market.fund(4e18)
+            ])
+            .then(values => {
+              console.info("[GnosisJS] > All done!");
+            })
+            .catch(error => {
+              console.warn(error);
+            });
+          })
+          .catch(error => {
+            console.warn(error);
+          });
+
+        })
+        .catch(error => {
+          console.warn(error);
+        });
+
+      })
+      .catch(error => {
+        console.warn(error);
+      });
+    })
+    .catch(error => {
+      console.warn(error);
+    });
+
+})
+.catch(error => {
+  console.warn('Make sure that Gnosis Development kit is up and running');
+});
+"""
+
+	with open("temp.js", "w") as f:
+		f.write(file_contents)
+
+	subprocess.run(["node", "temp.js"], shell=True, check=True)
+
+	return "Ongoing"
+
+
+
+
+
+
 @app.route('/convert_er/<val>/<_to>')
 def convert_eth_to_real(val, _to):
 	if request.method == 'GET':
